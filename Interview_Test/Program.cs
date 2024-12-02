6dd0450a5e87a9bae95b180d76b498cc517095eb
yousu <yousu@DESKTOP-MBKFQJ6>
2024-12-02 19:21:43 +0500

    Add project files.

---
+++ Interview_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>() { 1, 2, 2, 3, 3, 3 };
+        var numberCounts = Program.GetNumberCounts(numbers);
+        var formattedResult = string.Join(", ", numberCounts.Select(count => $"{count.Key}: {count.Value}"));
+        Console.WriteLine(formattedResult);
+    }
+
+
+    public static Dictionary<int, int> GetNumberCounts(List<int> numbers)
+    {
+        var numberCountDictionary = numbers
+            .GroupBy(number => number)
+            .ToDictionary(group => group.Key, group => group.Count());
+        return numberCountDictionary;
+    }
+}
